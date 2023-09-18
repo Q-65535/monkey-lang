2831f6ec481b2d11acd4e06e115fd03b1ad7b61f
Di Wu <cs.wudilet@gmail.com>
2023-09-19 01:57:24 +0800

    finish lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,132 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	readPosition    int
+	position        int  // current position, corresponds to current char
+	ch           byte  // current char
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input : input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	l.skipWhitespace()
+	switch l.ch {
+	case '=':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOTEQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			ident := l.readIdentifier()
+			tok = token.Token{Type: token.LookupIdent(ident), Literal: ident}
+			return tok
+		} else if isDigit(l.ch) {
+			tok = token.Token{Type: token.INT, Literal: l.readNumber()}
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition  >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
+}
+
+func (l *Lexer) readIdentifier() string {
+	startIndex := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[startIndex : l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	startIndex := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[startIndex : l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"monkey/lexer"
+	"monkey/token"
+	"monkey/repl"
+	"os"
+	// "os/user"
 )
 
 func main() {
-	fmt.Printf("hello world!");
+	l := lexer.New("hello+world;")
+	tok := l.NextToken()
+	for tok.Type != token.EOF {
+		fmt.Printf("%+v\n", tok)
+		tok = l.NextToken()
+	}
+	fmt.Printf("hello world!\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
